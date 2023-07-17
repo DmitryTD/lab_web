5b6fecbe0b093ea5a6851992c8f0fe455b20c991
DmitryTD <79697994+DmitryTD@users.noreply.github.com>
2023-07-17 16:49:04 +0300

    write main page and main.css

---
+++ src/index.php
@@ -0,0 +1,39 @@
+<!--   -->
+<!-- Подключаем основной стиль  -->
+<link rel="stylesheet" href="share/main.css">
+
+<header class="header">База данных "Картины"</header>
+<div class="navbar">
+    <a href="">Главная страница</a>
+
+    <div class="dropdown">
+        <button class="dropbtn"> База данных
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+        <a href="">О БД</a>
+        <a href="">Добавление данных</a>
+        <a href="">Поиск1</a>
+        <a href="">Поиск2</a>
+        <a href="">Поиск3</a>
+        <a href="">Статистика</a>
+        </div>
+    </div>
+    <a href="">XML</a>
+    <a href="">RSS</a>
+    <a href="">Виджеты</a>
+    <a href="">Галерея</a>
+    <a href="">Партнёры</a>
+    <a href="">О нас</a>
+    <a href="", style=float:right>Вход</a>
+    <a href="", style=float:right>Регистрация</a>
+</div>
+
+
+<footer class="footer">© 2023 Михальченко Дмитрий ИВТ-Б20 ИАТЭ НИЯУ МИФИ</footer>    
+<?php
+
+
+
+
+?>
